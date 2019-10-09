7f725f9cfb20f27895daa97d4ff980a35b3699af
Claudiu Gilmeanu <cgilmeanu@gmail.com>
2019-10-09 22:56:43 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "./components/header/club-header.component";
+import ClubHeader from "./components/header/club-header.component";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ClubHeader></ClubHeader>
+      <div className="App-container">
+        <div className="card-container">
+          <div className="box-item">
+            <div className="boardgames box-content"></div>
+            <div>
+              <span>Board Games Night</span>
+            </div>
+          </div>
+          <div className="box-item">
+            <div className="dungeon-and-dragons box-content"></div>
+            <div>
+              <span>Dungeon and Dragons (DnD) Night</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/header/club-header.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./club-header.component.css";
+
+function ClubHeader() {
+  return (
+    <header className="App-header">
+      <h1>Board Games Club</h1>
+    </header>
+  );
+}
+
+export default ClubHeader;

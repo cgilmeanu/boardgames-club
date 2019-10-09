fe9ad3d3b59b83b08197d42f59fae0b73d8a6f6b
Claudiu Gilmeanu <cgilmeanu@gmail.com>
2019-10-09 23:07:46 +0300

    Updates

---
+++ precache-manifest.20fb62346cc9cfb1d7f184086de6ed50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c239a724943ec2f2d9cabe646d3424d5",
+    "url": "/boardgames-club/index.html"
+  },
+  {
+    "revision": "40f4514b5d3ed26c5968",
+    "url": "/boardgames-club/static/css/main.55991cb0.chunk.css"
+  },
+  {
+    "revision": "bc24d37cdfe3db9ed8f7",
+    "url": "/boardgames-club/static/js/2.5a46359d.chunk.js"
+  },
+  {
+    "revision": "40f4514b5d3ed26c5968",
+    "url": "/boardgames-club/static/js/main.31e6f713.chunk.js"
+  },
+  {
+    "revision": "c3152c7f46f87f675d46",
+    "url": "/boardgames-club/static/js/runtime-main.2e392fc1.js"
+  },
+  {
+    "revision": "76ec0be15746b67fd6257cd601ff80a5",
+    "url": "/boardgames-club/static/media/clint-bustrillos-X-A-LJVAhzk-unsplash.76ec0be1.jpg"
+  },
+  {
+    "revision": "7161c93376ec69f3e3fd48345d877a21",
+    "url": "/boardgames-club/static/media/robert-coelho-laNNTAth9vs-unsplash.7161c933.jpg"
+  }
+]);
